10dd52c9fb94e228de5674d39e86a343e71f104f
MitchKoch <mitch@matternet.us>
2017-08-21 19:01:35 -0700

    Init.c for SPI supports 24Mhz crystal
    BOARD_CONFIG_MCU_STM32F302K8U6 changed to BOARD_CONFIG_MCU_STM32F3

--- src/init.c
+++ src/init.c
@@ -18,17 +18,19 @@
 #include <libopencm3/stm32/flash.h>
 #include <libopencm3/stm32/gpio.h>
 
-static void init_clock_stm32f302k8_8mhz_hse(void);
+static void init_clock_stm32f3_8mhz_hse(void);
+static void init_clock_stm32f3_24mhz_hse(void);
 
 void init_clock(void) {
-#if defined(BOARD_CONFIG_MCU_STM32F302K8U6) && defined(BOARD_CONFIG_OSC_HSE_8MHZ)
-    init_clock_stm32f302k8_8mhz_hse();
+#if defined(BOARD_CONFIG_MCU_STM32F3) && defined(BOARD_CONFIG_OSC_HSE_8MHZ)
+    init_clock_stm32f3_8mhz_hse();
+#elif defined(BOARD_CONFIG_MCU_STM32F3) && defined(BOARD_CONFIG_OSC_HSE_24MHZ)
 #else
     #error "Could not find valid clock config"
 #endif
 }
 
-static void init_clock_stm32f302k8_8mhz_hse(void)
+static void init_clock_stm32f3_8mhz_hse(void)
 {
     rcc_osc_on(RCC_HSE);
     rcc_wait_for_osc_ready(RCC_HSE);
@@ -38,6 +40,35 @@ static void init_clock_stm32f302k8_8mhz_hse(void)
     rcc_osc_off(RCC_PLL);
     rcc_wait_for_osc_not_ready(RCC_PLL);
     rcc_set_prediv(RCC_CFGR2_PREDIV_NODIV); // 8 Mhz
+    rcc_set_pll_source(RCC_CFGR_PLLSRC_HSE_PREDIV);
+    rcc_set_pll_multiplier(RCC_CFGR_PLLMUL_PLL_IN_CLK_X9); // 72 MHz
+
+    rcc_set_hpre(RCC_CFGR_HPRE_DIV_NONE); // 72 MHz
+    rcc_ahb_frequency = 72000000;
+
+    rcc_set_ppre1(RCC_CFGR_PPRE1_DIV_2); // 36 MHz
+    rcc_apb1_frequency = 36000000;
+
+    rcc_set_ppre2(RCC_CFGR_PPRE2_DIV_NONE); // 72 MHz
+    rcc_apb2_frequency = 72000000;
+
+    rcc_osc_on(RCC_PLL);
+    rcc_wait_for_osc_ready(RCC_PLL);
+    flash_set_ws(FLASH_ACR_PRFTBE | FLASH_ACR_LATENCY_2WS);
+    rcc_set_sysclk_source(RCC_CFGR_SW_PLL);
+    rcc_wait_for_sysclk_status(RCC_PLL);
+}
+
+static void init_clock_stm32f3_24mhz_hse(void)
+{
+    rcc_osc_on(RCC_HSE);
+    rcc_wait_for_osc_ready(RCC_HSE);
+    rcc_set_sysclk_source(RCC_CFGR_SW_HSE);
+    rcc_wait_for_sysclk_status(RCC_HSE);
+
+    rcc_osc_off(RCC_PLL);
+    rcc_wait_for_osc_not_ready(RCC_PLL);
+    rcc_set_prediv(RCC_CFGR2_PREDIV_DIV3); // 24 -> 8 Mhz
     rcc_set_pll_source(RCC_CFGR_PLLSRC_HSE_PREDIV);
     rcc_set_pll_multiplier(RCC_CFGR_PLLMUL_PLL_IN_CLK_X9); // 72 MHz
 
