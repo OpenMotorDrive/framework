b10e5b3e6a9619cfacffaae8c7193c79d703bd7e
bugobliterator <siddharthbharatpurohit@gmail.com>
2017-11-11 11:58:27 +0530

    use chibios sprintf for debug prints

--- modules/uavcan_debug/uavcan_debug.c
+++ modules/uavcan_debug/uavcan_debug.c
@@ -2,19 +2,21 @@
 #include <uavcan/uavcan.h>
 #include <stdio.h>
 #include <stdarg.h>
+#include <string.h>
+#include <chprintf.h>
 
 void uavcan_send_debug_msg(uint8_t debug_level, char* source, const char *fmt, ...)
 {
     va_list arg;
-    int done;
     struct uavcan_protocol_debug_LogMessage_s log_msg;
 
     va_start (arg, fmt);
-    log_msg.text_len =  vsprintf(log_msg.text, fmt, arg);
+    log_msg.text_len =  chsnprintf(log_msg.text, sizeof(log_msg.text),fmt, arg);
     va_end (arg);
 
-    memcpy(log_msg.source, source, sizeof(source));
-    log_msg.source_len = sizeof(source);
+    log_msg.source_len = strlen(source);
+
+    memcpy(log_msg.source, source, log_msg.source_len);
     log_msg.level.value = debug_level;
     uavcan_broadcast(0, &uavcan_protocol_debug_LogMessage_descriptor, CANARD_TRANSFER_PRIORITY_HIGH, &log_msg);
 }
