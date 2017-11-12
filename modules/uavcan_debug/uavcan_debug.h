c95feb89fb2c09ab3aaa4aedaaaa75e407e6b8c3
bugobliterator <siddharthbharatpurohit@gmail.com>
2017-11-12 16:19:58 +0530

    uavcan_debug: add key value send method

--- modules/uavcan_debug/uavcan_debug.h
+++ modules/uavcan_debug/uavcan_debug.h
@@ -1,5 +1,7 @@
 #pragma once
 
 #include <uavcan.protocol.debug.LogMessage.h>
+#include <uavcan.protocol.debug.KeyValue.h>
 
 void uavcan_send_debug_msg(uint8_t debug_level, char* source, const char *fmt, ...);
+void uavcan_send_debug_keyvalue(char* key, float value);
