67aa147382eda043b398d6fdb500cc52b884775a
bugobliterator <siddharthbharatpurohit@gmail.com>
2017-11-12 16:19:12 +0530

    uavcan_debug: use chibios printf method for debug prints

--- modules/uavcan_debug/uavcan_debug.c
+++ modules/uavcan_debug/uavcan_debug.c
@@ -1,18 +1,32 @@
 #include "uavcan_debug.h"
-#include <uavcan/uavcan.h>
+#include <modules/uavcan/uavcan.h>
 #include <stdio.h>
 #include <stdarg.h>
 #include <string.h>
 #include <chprintf.h>
-
+#include <memstreams.h>
 void uavcan_send_debug_msg(uint8_t debug_level, char* source, const char *fmt, ...)
 {
-    va_list arg;
     struct uavcan_protocol_debug_LogMessage_s log_msg;
 
-    va_start (arg, fmt);
-    log_msg.text_len =  chsnprintf(log_msg.text, sizeof(log_msg.text),fmt, arg);
-    va_end (arg);
+    va_list ap;
+    MemoryStream ms;
+    BaseSequentialStream *chp;
+
+    /* Memory stream object to be used as a string writer, reserving one
+     byte for the final zero.*/
+    msObjectInit(&ms, (uint8_t *)log_msg.text, sizeof(log_msg.text), 0);
+
+    /* Performing the print operation using the common code.*/
+    chp = (BaseSequentialStream *)(void *)&ms;
+    va_start(ap, fmt);
+    log_msg.text_len = chvprintf(chp, fmt, ap);
+    va_end(ap);
+
+    /* Terminate with a zero, unless size==0.*/
+    if (ms.eos < sizeof(log_msg.text))
+      log_msg.text[ms.eos] = 0;
+
 
     log_msg.source_len = strlen(source);
 
