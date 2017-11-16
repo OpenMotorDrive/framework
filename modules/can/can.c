e972a7282fbf425fbcee5633403f64891c5fc101
Jonathan Challinger <mr.challinger@gmail.com>
2017-11-15 19:53:03 -0800

    add BOARD_CAN_CONFIG macro

--- modules/can/can.c
+++ modules/can/can.c
@@ -18,11 +18,7 @@ static CANDriver* can_get_device(uint8_t can_idx) {
 }
 
 void can_init(uint8_t can_idx, uint32_t baudrate, bool silent) {
-    const CANConfig cancfg = {
-        CAN_MCR_ABOM | CAN_MCR_AWUM | CAN_MCR_TXFP,
-        (silent?CAN_BTR_SILM:0) | CAN_BTR_SJW(0) | CAN_BTR_TS2(2-1) |
-        CAN_BTR_TS1(15-1) | CAN_BTR_BRP((STM32_PCLK1/18)/baudrate - 1)
-    };
+    CANConfig cancfg = BOARD_CAN_CONFIG(baudrate, silent);
 
     canStart(can_get_device(can_idx), &cancfg);
 
--- modules/platform_stm32f302x8/platform_stm32f302x8.h
+++ modules/platform_stm32f302x8/platform_stm32f302x8.h
@@ -15,6 +15,12 @@ extern uint8_t _param1_flash_sec_end;
 extern uint8_t _param2_flash_sec;
 extern uint8_t _param2_flash_sec_end;
 
+#define BOARD_CAN_CONFIG(baudrate, silent) { \
+CAN_MCR_ABOM | CAN_MCR_AWUM | CAN_MCR_TXFP, \
+(silent?CAN_BTR_SILM:0) | CAN_BTR_SJW(0) | CAN_BTR_TS2(2-1) | \
+CAN_BTR_TS1(15-1) | CAN_BTR_BRP((STM32_PCLK1/18)/baudrate - 1) \
+}
+
 void board_get_unique_id(uint8_t* buf, uint8_t len);
 
 #if !defined(_FROM_ASM_)
