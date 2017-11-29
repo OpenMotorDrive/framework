e5b01b98f87adad5a77a291dd5b13444c521395d
Jonathan Challinger <mr.challinger@gmail.com>
2017-11-28 17:51:08 -0800

    uavcan_getnodeinfo_server: update to reflect worker thread api change

--- modules/uavcan_getnodeinfo_server/uavcan_getnodeinfo_server.c
+++ modules/uavcan_getnodeinfo_server/uavcan_getnodeinfo_server.c
@@ -13,14 +13,12 @@
 
 #include <uavcan.protocol.GetNodeInfo.h>
 
-static struct pubsub_listener_s getnodeinfo_req_listener;
 static struct worker_thread_listener_task_s getnodeinfo_req_listener_task;
 static void getnodeinfo_req_handler(size_t msg_size, const void* buf, void* ctx);
 
 RUN_AFTER(UAVCAN_INIT) {
     struct pubsub_topic_s* getnodeinfo_req_topic = uavcan_get_message_topic(0, &uavcan_protocol_GetNodeInfo_req_descriptor);
-    pubsub_init_and_register_listener(getnodeinfo_req_topic, &getnodeinfo_req_listener, getnodeinfo_req_handler, NULL);
-    worker_thread_add_listener_task(&lpwork_thread, &getnodeinfo_req_listener_task, &getnodeinfo_req_listener);
+    worker_thread_add_listener_task(&lpwork_thread, &getnodeinfo_req_listener_task, getnodeinfo_req_topic, getnodeinfo_req_handler, NULL);
 }
 
 static void getnodeinfo_req_handler(size_t msg_size, const void* buf, void* ctx) {
