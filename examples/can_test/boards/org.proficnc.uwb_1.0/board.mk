25dc711947a629c19feff787fade2f7c29ae2d3e
Jonathan Challinger <mr.challinger@gmail.com>
2017-12-04 11:29:49 -0800

    remove examples/can_test

--- examples/can_test/boards/org.proficnc.uwb_1.0/board.mk
+++
@@ -1,4 +0,0 @@
-BOARD_DIR := $(patsubst %/,%,$(dir $(lastword $(MAKEFILE_LIST))))
-BOARD_SRC = $(BOARD_DIR)/board.c
-BOARD_INC = $(BOARD_DIR)
-MODULES_ENABLED += platform_stm32f302x8
--- examples/can_test/src/worker_threads.c
+++
@@ -1,3 +0,0 @@
-#include <modules/worker_thread/worker_thread.h>
-
-WORKER_THREAD_CREATE(lpwork_thread, , )
