c64d170759bbc345ad018e60116ba28046bbb493
Siddharth Purohit <siddharthbharatpurohit@gmail.com>
2017-12-05 21:46:09 +0530

    ext_irq: make driver instance static

--- modules/ext_irq/ext_irq.c
+++ modules/ext_irq/ext_irq.c
@@ -1,6 +1,7 @@
 
 
 #include <hal.h>
+#include <ch.h>
 #include "ext_irq.h"
 #include <modules/pubsub/pubsub.h>
 #include <common/ctor.h>
@@ -19,7 +20,7 @@ struct ext_irq_instance_s {
     EXTDriver *drv;
     struct ext_irq_topic_list_s *ext_irq_list_head;
     mutex_t lock;
-} *exti_instance = NULL;
+} exti_instance;
 
 static EXTConfig extcfg = {
   {
@@ -61,15 +62,14 @@ static void ext_irq_common_handler(EXTDriver *extp, expchannel_t channel)
 {
     (void)extp;
     static volatile uint16_t irq_cnt;
-    if (exti_instance == NULL) {
-        return;
-    }
 
     struct ext_irq_topic_list_s* ext_irq_list_item;
-    ext_irq_list_item = exti_instance->ext_irq_list_head;
+    ext_irq_list_item = exti_instance.ext_irq_list_head;
     while(ext_irq_list_item) {
         if (ext_irq_list_item->channel == channel) {
-            worker_thread_publisher_task_publish_from_ISR(&ext_irq_list_item->task, 0, NULL, NULL);
+            chSysLockFromISR();
+            worker_thread_publisher_task_publish_I(&ext_irq_list_item->task, &ext_irq_list_item->topic, 0, NULL, NULL);
+            chSysUnlockFromISR();
         }
         ext_irq_list_item = ext_irq_list_item->next;
     }
@@ -83,23 +83,16 @@ RUN_AFTER(CH_SYS_INIT) {
 
 static void ext_irq_init(void)
 {
-    exti_instance = chHeapAllocAligned(NULL, sizeof(struct ext_irq_instance_s), 8);
-    if (!(exti_instance = chCoreAllocAligned(sizeof(struct ext_irq_instance_s), PORT_WORKING_AREA_ALIGN))) { goto fail; }
-    memset(exti_instance, 0, sizeof(struct ext_irq_instance_s));
-    chMtxObjectInit(&exti_instance->lock);
-    exti_instance->drv = &EXTD1;
+    memset(&exti_instance, 0, sizeof(struct ext_irq_instance_s));
+    chMtxObjectInit(&exti_instance.lock);
+    exti_instance.drv = &EXTD1;
     return;
-fail:
-    chSysHalt("EXT IRQ Init Failed!");
 }
 
 struct pubsub_topic_s* enable_ext_irq(uint32_t gpio_port, uint8_t pin_int_num, uint8_t mode)
 {
-    if(!exti_instance) {
-        return NULL;
-    }
-    chMtxLock(&exti_instance->lock);
-    extStop(exti_instance->drv);
+    chMtxLock(&exti_instance.lock);
+    extStop(exti_instance.drv);
     switch(mode) {
         case FW_EXT_IRQ_LOW_LEVEL:
             extcfg.channels[pin_int_num].mode = EXT_CH_MODE_LOW_LEVEL;
@@ -117,7 +110,7 @@ struct pubsub_topic_s* enable_ext_irq(uint32_t gpio_port, uint8_t pin_int_num, u
     extcfg.channels[pin_int_num].mode |= EXT_CH_MODE_AUTOSTART | gpio_port;
 
     //Append irq topic request
-    struct ext_irq_topic_list_s* ext_irq_list_item = exti_instance->ext_irq_list_head;
+    struct ext_irq_topic_list_s* ext_irq_list_item = exti_instance.ext_irq_list_head;
     while (ext_irq_list_item && ext_irq_list_item->channel != pin_int_num) {
         ext_irq_list_item = ext_irq_list_item->next;
     }
@@ -135,24 +128,21 @@ struct pubsub_topic_s* enable_ext_irq(uint32_t gpio_port, uint8_t pin_int_num, u
 
     worker_thread_add_publisher_task(&WT, &ext_irq_list_item->task, 0, 2);
 
-    LINKED_LIST_APPEND(struct ext_irq_topic_list_s, exti_instance->ext_irq_list_head, ext_irq_list_item);
+    LINKED_LIST_APPEND(struct ext_irq_topic_list_s, exti_instance.ext_irq_list_head, ext_irq_list_item);
 
-    chMtxUnlock(&exti_instance->lock);
+    chMtxUnlock(&exti_instance.lock);
 
 start_and_return:
     extcfg.channels[pin_int_num].cb = ext_irq_common_handler;
-    extStart(exti_instance->drv, &extcfg);
+    extStart(exti_instance.drv, &extcfg);
     return &ext_irq_list_item->topic;
 }
 
 void disable_ext_irq(uint8_t pin_int_num)
 {
-    if(!exti_instance) {
-        return;
-    }
-    extStop(exti_instance->drv);
+    extStop(exti_instance.drv);
     extcfg.channels[pin_int_num].mode = EXT_CH_MODE_DISABLED;
     extcfg.channels[pin_int_num].cb = NULL;
     //TODO: free allocated topic as well
-    extStart(exti_instance->drv, &extcfg);
+    extStart(exti_instance.drv, &extcfg);
 }
