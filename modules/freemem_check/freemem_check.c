afcd842f1cf396d915eb86901f4ba8dea028f935
Jonathan Challinger <mr.challinger@gmail.com>
2017-12-13 09:00:32 -0800

    add freemem_check module

---
+++ modules/freemem_check/freemem_check.c
@@ -0,0 +1,20 @@
+#include <common/ctor.h>
+#include <ch.h>
+
+#ifdef MODULE_UAVCAN_DEBUG_ENABLED
+#include <modules/uavcan_debug/uavcan_debug.h>
+#endif
+
+#ifndef REQUIRED_RAM_MARGIN_AFTER_INIT
+#error Please define REQUIRED_RAM_MARGIN_AFTER_INIT (bytes) in framework_conf.h.
+#endif
+
+RUN_AFTER(INIT_END) {
+    if (chCoreGetStatusX() < REQUIRED_RAM_MARGIN_AFTER_INIT) {
+        chSysHalt(NULL);
+    }
+
+#ifdef MODULE_UAVCAN_DEBUG_ENABLED
+    uavcan_send_debug_msg(UAVCAN_PROTOCOL_DEBUG_LOGLEVEL_INFO, "", "freemem %u", chCoreGetStatusX());
+#endif
+}
--- src/warnings.c
+++ src/warnings.c
@@ -15,3 +15,7 @@
 #if !CH_DBG_ENABLE_STACK_CHECK
 #warning Consider enabling CH_DBG_ENABLE_STACK_CHECK in framework_conf.h.
 #endif
+
+#ifndef MODULE_FREEMEM_CHECK_ENABLED
+#warning Consider enabling module freemem_check.
+#endif
