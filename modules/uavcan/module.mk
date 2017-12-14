bdfe96fb5d68e3678857cfd6814eb2322398c063
Jonathan Challinger <mr.challinger@gmail.com>
2017-12-13 19:17:21 -0800

    fix bad dependencies caused by gnu find with no args

--- modules/uavcan/module.mk
+++ modules/uavcan/module.mk
@@ -6,11 +6,11 @@ UDEFS += -D"CANARD_ASSERT(x)"="{}"
 
 DSDL_NAMESPACE_DIR_UAVCAN ?= $(FRAMEWORK_DIR)/dsdl/uavcan
 
-DSDL_NAMESPACE_DIRS += $(DSDL_NAMESPACE_DIR_UAVCAN)
+DSDL_NAMESPACE_DIRS += $(DSDL_NAMESPACE_DIR_UAVCAN) $(VENDOR_DSDL_NAMESPACE_DIRS)
 
 INCDIR += $(BUILDDIR)/dsdlc/include
 
-$(BUILDDIR)/dsdlc.mk: $(shell find $(DSDL_NAMESPACE_DIRS)) $(shell find $(VENDOR_DSDL_NAMESPACE_DIRS))
+$(BUILDDIR)/dsdlc.mk: $(foreach dsdl_dir,$(wildcard $(DSDL_NAMESPACE_DIRS)),$(shell find $(dsdl_dir)))
 	rm -rf $(BUILDDIR)/dsdlc
 	python $(UAVCAN_MODULE_DIR)/canard_dsdlc/canard_dsdlc.py $(addprefix --build=,$(MESSAGES_ENABLED)) $(DSDL_NAMESPACE_DIRS) $(VENDOR_DSDL_NAMESPACE_DIRS) $(BUILDDIR)/dsdlc
 	find $(BUILDDIR)/dsdlc/src -name "*.c" | xargs echo CSRC += > $(BUILDDIR)/dsdlc.mk
