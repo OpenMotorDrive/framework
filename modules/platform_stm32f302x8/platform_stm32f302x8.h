8190822aa8a0e33b091e6edb260ae3c0273aa832
Jonathan Challinger <mr.challinger@gmail.com>
2017-12-29 10:48:10 -0800

    platform_stm32f302x8: remove pal_default_config

--- modules/platform_stm32f302x8/platform_stm32f302x8.h
+++ modules/platform_stm32f302x8/platform_stm32f302x8.h
@@ -15,12 +15,6 @@ extern uint8_t _param1_flash_sec_end;
 extern uint8_t _param2_flash_sec;
 extern uint8_t _param2_flash_sec_end;
 
-#define BOARD_CAN_CONFIG(baudrate, silent) { \
-CAN_MCR_ABOM | CAN_MCR_AWUM, \
-(silent?CAN_BTR_SILM:0) | CAN_BTR_SJW(0) | CAN_BTR_TS2(2-1) | \
-CAN_BTR_TS1(15-1) | CAN_BTR_BRP((STM32_PCLK1/18)/baudrate - 1) \
-}
-
 void board_get_unique_id(uint8_t* buf, uint8_t len);
 
 #if !defined(_FROM_ASM_)
