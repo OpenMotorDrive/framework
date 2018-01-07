5f4debf4e1ae543a865fe4587d665d8de9e135c6
Jonathan Challinger <mr.challinger@gmail.com>
2018-01-06 19:03:30 -0800

    examples/basic_uavcan_functionality: add support for the cube

---
+++ examples/basic_uavcan_functionality/boards/com.hex.cube_1.0/board.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <stdint.h>
+#include <modules/platform_stm32f427/platform_stm32f427.h>
+
+#define BOARD_PAL_LINE_CAN_RX PAL_LINE(GPIOD,0)
+#define BOARD_PAL_LINE_CAN_TX PAL_LINE(GPIOD,1)
