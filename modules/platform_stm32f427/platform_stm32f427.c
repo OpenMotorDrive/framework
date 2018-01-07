ac310dd1e060ae00ef3edbbb59b2393baeb4da12
Jonathan Challinger <mr.challinger@gmail.com>
2018-01-06 19:00:51 -0800

    add stm32f427 platform

---
+++ modules/platform_stm32f427/platform_stm32f427.c
@@ -0,0 +1,26 @@
+#include "platform_stm32f427.h"
+#include <hal.h>
+#include <string.h>
+
+/**
+ * @brief   Early initialization code.
+ * @details This initialization must be performed just after stack setup
+ *          and before any other initialization.
+ */
+void __early_init(void) {
+    stm32_clock_init();
+}
+
+void board_get_unique_id(uint8_t* buf, uint8_t len) {
+    uint32_t unique_id_uint32[3];
+    unique_id_uint32[0] = ((uint32_t*)0x1FFF7A10)[2];
+    unique_id_uint32[1] = ((uint32_t*)0x1FFF7A10)[1];
+    unique_id_uint32[2] = ((uint32_t*)0x1FFF7A10)[0];
+
+    if (len>12) {
+        memset(buf, 0, len);
+        memcpy(buf, unique_id_uint32, 12);
+    } else {
+        memcpy(buf, unique_id_uint32, len);
+    }
+}
