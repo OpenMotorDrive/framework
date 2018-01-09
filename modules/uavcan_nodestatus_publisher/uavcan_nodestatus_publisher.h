adde3ae563be9db5720bad5f80a9ad2ff55aa4df
Siddharth Purohit <siddharthbharatpurohit@gmail.com>
2018-01-10 00:11:38 +0530

    node_status: add node health and mode setting

--- modules/uavcan_nodestatus_publisher/uavcan_nodestatus_publisher.h
+++ modules/uavcan_nodestatus_publisher/uavcan_nodestatus_publisher.h
@@ -3,3 +3,5 @@
 #include <uavcan.protocol.NodeStatus.h>
 
 const struct uavcan_protocol_NodeStatus_s* uavcan_nodestatus_publisher_get_nodestatus_message(void);
+void set_node_health(uint8_t health);
+void set_node_mode(uint8_t mode);
