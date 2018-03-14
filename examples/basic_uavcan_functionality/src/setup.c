ef88c038174f06a9a6c2553b40e38b143201872a
Mark Whitehorn <kd0aij@gmail.com>
2018-03-14 16:17:15 -0600

    revert can thread stack and pubsub topic sizes to original

--- examples/basic_uavcan_functionality/src/setup.c
+++ examples/basic_uavcan_functionality/src/setup.c
@@ -2,6 +2,6 @@
 #include <modules/worker_thread/worker_thread.h>
 
 WORKER_THREAD_TAKEOVER_MAIN(lpwork_thread, LOWPRIO)
-WORKER_THREAD_SPAWN(can_thread, LOWPRIO, 4096)
+WORKER_THREAD_SPAWN(can_thread, LOWPRIO, 1024)
 
-PUBSUB_TOPIC_GROUP_CREATE(default_topic_group, 2048)
+PUBSUB_TOPIC_GROUP_CREATE(default_topic_group, 1024)
