b97ee0e12bc0bf00eb8586938c3113b039ca6aee
Mark Whitehorn <kd0aij@gmail.com>
2018-03-14 16:39:05 -0600

    increase can_thread stack size to 2KiB
    stack check reported 1440 (>1024) bytes free with stack size of 1024 but
    only 928 bytes free with size 1536.

--- examples/basic_uavcan_functionality/src/setup.c
+++ examples/basic_uavcan_functionality/src/setup.c
@@ -2,6 +2,6 @@
 #include <modules/worker_thread/worker_thread.h>
 
 WORKER_THREAD_TAKEOVER_MAIN(lpwork_thread, LOWPRIO)
-WORKER_THREAD_SPAWN(can_thread, LOWPRIO, 1024)
+WORKER_THREAD_SPAWN(can_thread, LOWPRIO, 2048)
 
 PUBSUB_TOPIC_GROUP_CREATE(default_topic_group, 1024)
