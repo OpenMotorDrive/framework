e523d72c10b7cac11114dc1b55f53ebee9a74fae
Siddharth Purohit <siddharthbharatpurohit@gmail.com>
2018-03-21 21:00:29 +0530

    bootloader: fix build for stm32f3 controllers

--- examples/driver_invensense/boards/com.hex.flow_1.0/board.h
+++ examples/driver_invensense/boards/com.hex.flow_1.0/board.h
@@ -2,6 +2,19 @@
 
 #include <modules/platform_stm32f302x8/platform_stm32f302x8.h>
 
+
+#define BOARD_CONFIG_HW_NAME "org.proficnc.flow"
+#define BOARD_CONFIG_HW_MAJOR_VER 1
+#define BOARD_CONFIG_HW_MINOR_VER 0
+
+#define BOARD_CONFIG_HW_INFO_STRUCTURE { \
+    .hw_name = BOARD_CONFIG_HW_NAME, \
+    .hw_major_version = BOARD_CONFIG_HW_MAJOR_VER, \
+    .hw_minor_version = BOARD_CONFIG_HW_MINOR_VER, \
+    .board_desc_fmt = SHARED_HW_INFO_BOARD_DESC_FMT_NONE, \
+    .board_desc = 0, \
+}
+
 #define BOARD_PAL_LINE_SPI3_SCK PAL_LINE(GPIOB,3)
 #define BOARD_PAL_LINE_SPI3_MISO PAL_LINE(GPIOB,4)
 #define BOARD_PAL_LINE_SPI3_MOSI PAL_LINE(GPIOB,5)
