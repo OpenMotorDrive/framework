43f1c21965fa04b18bc2bb5af72eccea7cfef509
Pierre Kancir <pierre.kancir@azurdrones.com>
2018-04-03 10:20:39 +0200

    tests

--- examples/driver_usb/src/usb_test.c
+++ examples/driver_usb/src/usb_test.c
@@ -16,11 +16,11 @@
 #define WT hpwork_thread
 WORKER_THREAD_DECLARE_EXTERN(WT)
 
-static struct worker_thread_timer_task_s blink_led_green_task;
-static struct worker_thread_timer_task_s blink_led_red_task;
-
-static void blink_led_green_task_func(struct worker_thread_timer_task_s* task);
-static void blink_led_red_task_func(struct worker_thread_timer_task_s* task);
+//static struct worker_thread_timer_task_s blink_led_green_task;
+//static struct worker_thread_timer_task_s blink_led_red_task;
+//
+//static void blink_led_green_task_func(struct worker_thread_timer_task_s* task);
+//static void blink_led_red_task_func(struct worker_thread_timer_task_s* task);
 
 // handle request to change blink speed on green led
 void cmd_blinkspeed(BaseSequentialStream *chp, int argc, char *argv[]) {
@@ -38,7 +38,7 @@ void cmd_blinkspeed(BaseSequentialStream *chp, int argc, char *argv[]) {
     } else if(millis < 5) {
         millis = 5;
     }
-    worker_thread_timer_task_reschedule(&WT, &blink_led_green_task, MS2ST(millis));
+    //worker_thread_timer_task_reschedule(&WT, &blink_led_green_task, MS2ST(millis));
 }
 
 static const ShellCommand commands[] = {
@@ -50,44 +50,44 @@ static const ShellConfig shell_cfg1 = {
   commands
 };
 
-//#define SHELL_WA_SIZE   THD_WORKING_AREA_SIZE(2048)
-//
-//static THD_WORKING_AREA(waShellThread, 2048);
-//static THD_FUNCTION(ShellThread, arg) {
-//    (void)arg;
-//    chRegSetThreadName("shellthread");
-//    while (true) {
-//        if (SDU1.config->usbp->state == USB_ACTIVE) {
-//            thread_t *shelltp = chThdCreateFromHeap(NULL, SHELL_WA_SIZE,
-//                                                    "shell", NORMALPRIO + 1,
-//                                                    shellThread, (void *)&shell_cfg1);
-//            chThdWait(shelltp);               /* Waiting termination.             */
-//        }
-//        chThdSleepMilliseconds(1000);
-//    }
-//}
+#define SHELL_WA_SIZE   THD_WORKING_AREA_SIZE(2048)
+
+static THD_WORKING_AREA(waShellThread, 2048);
+static THD_FUNCTION(ShellThread, arg) {
+    (void)arg;
+    chRegSetThreadName("shellthread");
+    while (true) {
+        if (SDU1.config->usbp->state == USB_ACTIVE) {
+            thread_t *shelltp = chThdCreateFromHeap(NULL, SHELL_WA_SIZE,
+                                                    "shell", NORMALPRIO + 1,
+                                                    shellThread, (void *)&shell_cfg1);
+            chThdWait(shelltp);               /* Waiting termination.             */
+        }
+        chThdSleepMilliseconds(1000);
+    }
+}
 
 // Init the USB
-//void usb_init(void) {
-//    sduObjectInit(&SDU1);
-//    sduStart(&SDU1, &serusbcfg);
-//    /*
-//    * Activates the USB driver and then the USB bus pull-up on D+.
-//    * Note, a delay is inserted in order to not have to disconnect the cable
-//    * after a reset.
-//    */
-//    usbDisconnectBus(serusbcfg.usbp);
-//    chThdSleepMilliseconds(1500);
-//    usbStart(serusbcfg.usbp, &usbcfg);
-//    usbConnectBus(serusbcfg.usbp);
-//    /*
-//     * Shell manager initialization.
-//     */
-//    shellInit();
-//    chThdCreateStatic(waShellThread, sizeof(waShellThread), LOWPRIO,
-//                      ShellThread, NULL);
-//
-//}
+void usb_init(void) {
+    sduObjectInit(&SDU1);
+    sduStart(&SDU1, &serusbcfg);
+    /*
+    * Activates the USB driver and then the USB bus pull-up on D+.
+    * Note, a delay is inserted in order to not have to disconnect the cable
+    * after a reset.
+    */
+    usbDisconnectBus(serusbcfg.usbp);
+    chThdSleepMilliseconds(1500);
+    usbStart(serusbcfg.usbp, &usbcfg);
+    usbConnectBus(serusbcfg.usbp);
+    /*
+     * Shell manager initialization.
+     */
+    shellInit();
+    chThdCreateStatic(waShellThread, sizeof(waShellThread), NORMALPRIO+1,
+                      ShellThread, NULL);
+
+}
 
 // Hold green led state
 bool led_green_on;
@@ -96,36 +96,37 @@ bool led_red_on;
 
 // Function call after the end of init
 RUN_AFTER(INIT_END) {
-//    usb_init();
-    led_green_on = false;
-    led_red_on = false;
+    usb_init();
+    //led_green_on = false;
+    //led_red_on = false;
+   // palSetPad(GPIOG, GPIOG_LED3_GREEN);
     // Add new tasks to worker thread
     // make led blink and report every 1000ms (repeated)
-    worker_thread_add_timer_task(&WT, &blink_led_green_task, blink_led_green_task_func, NULL, MS2ST(1000), true);
-    worker_thread_add_timer_task(&WT, &blink_led_red_task, blink_led_red_task_func, NULL, MS2ST(2000), true);
+    //worker_thread_add_timer_task(&WT, &blink_led_green_task, blink_led_green_task_func, NULL, MS2ST(1000), true);
+    //worker_thread_add_timer_task(&WT, &blink_led_red_task, blink_led_red_task_func, NULL, MS2ST(2000), true);
 }
 
 
-static void blink_led_green_task_func(struct worker_thread_timer_task_s* task) {
-    if (led_green_on) {
-        palClearPad(GPIOG, GPIOG_LED3_GREEN);
-        led_green_on = false;
-    } else {
-        palSetPad(GPIOG, GPIOG_LED3_GREEN);
-        led_green_on = true;
-    }
-    //uavcan_send_debug_keyvalue("LG", (unsigned long)chVTGetSystemTime());
-}
-
-
-static void blink_led_red_task_func(struct worker_thread_timer_task_s* task) {
-    if (led_red_on) {
-        palClearPad(GPIOG, GPIOG_LED4_RED);
-        led_red_on = false;
-    } else {
-        palSetPad(GPIOG, GPIOG_LED4_RED);
-        led_red_on = true;
-    }
-    //uavcan_send_debug_keyvalue("LR", (unsigned long)chVTGetSystemTime());
-}
+//static void blink_led_green_task_func(struct worker_thread_timer_task_s* task) {
+//    if (led_green_on) {
+//        palClearPad(GPIOG, GPIOG_LED3_GREEN);
+//        led_green_on = false;
+//    } else {
+//        palSetPad(GPIOG, GPIOG_LED3_GREEN);
+//        led_green_on = true;
+//    }
+//    //uavcan_send_debug_keyvalue("LG", (unsigned long)chVTGetSystemTime());
+//}
+//
+//
+//static void blink_led_red_task_func(struct worker_thread_timer_task_s* task) {
+//    if (led_red_on) {
+//        palClearPad(GPIOG, GPIOG_LED4_RED);
+//        led_red_on = false;
+//    } else {
+//        palSetPad(GPIOG, GPIOG_LED4_RED);
+//        led_red_on = true;
+//    }
+//    //uavcan_send_debug_keyvalue("LR", (unsigned long)chVTGetSystemTime());
+//}
 
